c789e1dbe0d9a3a75e4e84226b910f53f20d050a
shahabsaqib220 <shahabsaqib220@gmail.com>
2024-12-07 14:31:23 +0500

    Delete add Module has been added

--- modules/user-existing-edited-ad/main-router.js
+++ modules/user-existing-edited-ad/main-router.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth-middleware")
 
-const {update_user_edited_ad, upload} = require("./router");
+const {update_user_edited_ad, upload, delete_user_ad} = require("./router");
 
 
 
 router.put("/ad/:id",authMiddleware, upload.array('images', 5),update_user_edited_ad );
+router.delete("/delete/user/ad/:id",authMiddleware, delete_user_ad);
 
 
 
 module.exports = router;
-
 
--- modules/user-existing-edited-ad/router.js
+++ modules/user-existing-edited-ad/router.js
@@ -117,6 +117,32 @@ const update_user_edited_ad = async (req, res) => {
   }
 };
 
+
+const delete_user_ad = async (req,res) =>{
+  const adId = req.params.id;
+
+  try {
+      
+      const ad = await Ad.findOne({ _id: adId }); 
+
+      if (!ad) {
+          return res.status(404).json({ message: "Ad not found!" });
+      }
+
+     
+      await Ad.findByIdAndDelete(adId);
+      return res.status(200).json({ message: "Ad has been deleted" });
+
+  } catch (error) {
+      return res.status(500).json({ message: "Server Error", error });
+  }
+};
+
+
+
+
+
+
 module.exports = {
-    update_user_edited_ad, upload
+    update_user_edited_ad, upload, delete_user_ad
 };
